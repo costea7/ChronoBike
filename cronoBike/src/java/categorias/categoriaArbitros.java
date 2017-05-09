88d2078dc8712fb84a9bef35493e19683c67d46c
Iulian <Iulian@10.7.1.172>
2017-05-09 16:52:19 +0200

    Se han creado las Entity Class de la BD y Enum de los distintos tipos de categorias... (segunda parte)

---
+++ cronoBike/src/java/categorias/categoriaArbitros.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package categorias;
+
+/**
+ *
+ * @author Iulian
+ */
+public enum categoriaArbitros {
+    
+    ASPIRANTE_JUVENIL, ASPIRANTE, AUTONOMICO, NACIONAL, NACIONAL_ELITE, INTERNACIONAL_UCI
+    
+}
---
+++ cronoBike/src/java/categorias/categoriaCorredores.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package categorias;
+
+/**
+ *
+ * @author Iulian
+ */
+public enum categoriaCorredores {
+    
+    PROMESA, PRINCIPIANTE, ALEVIN, INFANTIL, CADETE, F_CAD, JUNIOR, F_JUN, 
+    ELITE, SUB23, ELITE_UCI, F_ELITE, F_SUB23, F_ELITE_UCI, MASTER_30, 
+    MASTER_40, MASTER_50, MASTER_60, F_MASTER_30, F_MASTER_50, F_MASTER_60,
+    CICLOTURISTA, F_CICLOTURISTA
+        
+}
